c71b1f89b038cb437da8e5a91462e5d1a410fed0
Amar Patel <theamarpatel@gmail.com>
2014-05-19 14:26:54 -0700

    indicators dynamically tile and resize

--- ToDo.js
+++ ToDo.js
@@ -1,35 +1,20 @@
 ToDo
-  -css/style toggle for effect
-    based on time
   -for user to add new indicator, have a view appear
   -have ~10 already running
   -figure out arragement of indicators
-  -current smallest step:
-    create one that works!
+  -on mouseover: show source
+  // -create one that works!
   // -make DB!
   //   -have input fields post to sql server
-  -figure out how to add to indicators to
-    list when added
+  // -Using mongo data, create an indicator with 
+  //   flashing based on indicator.frequency
 
 
 NOTES
 
-  for flashes:
-    interval(function(){
-    timeout(whatevertime function({
-    backtotopTOcallinterval?
-    }))
-    })
-
-Features:
-
-  -on mouseover: show source
-
-
-
 Current Task
-  -Using mongo data, create an indicator with 
-    flashing based on indicator.frequency
+  -figure out how to add to indicators as
+   squares that tile and sort dynamically  
 
 
 
